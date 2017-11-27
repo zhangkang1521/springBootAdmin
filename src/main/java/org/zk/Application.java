d80d923ad5b57d3e07130c2a44757ec679407c40
zhangkang <zhangkang@lvmama.com>
2017-11-27 19:09:59 +0800

    admin

--- src/main/java/org/zk/Application.java
+++ src/main/java/org/zk/Application.java
@@ -1,12 +1,16 @@
 package org.zk;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by zhangkang on 2017/11/27.
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class Application {
 
     public static void main(String[] args) throws Exception {
