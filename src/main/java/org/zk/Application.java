f63d36e852d82f0a2b6a1d822ff3abebf151a17e
zhangkang <zhangkang@lvmama.com>
2017-11-27 16:43:06 +0800

    spring boot init

---
+++ src/main/java/org/zk/Application.java
@@ -0,0 +1,15 @@
+package org.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhangkang on 2017/11/27.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
